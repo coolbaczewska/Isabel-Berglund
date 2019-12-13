cd0913830aab3dda88dee8d354fce8b621d18410
Toby Van Waale <toby@Tobys-MacBook-Pro.local>
2019-12-13 19:34:38 +0100

    json

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", getData)
+
+function getData(){
+    console.log("getData")
+    //fetch("http://georgianadancu.com/wordpress/wp-json/wp/v2/movies?_embed")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/project?_embed")
+    .then(res=>res.json())
+    .then(handleData)
+}
+
+function handleData(myData){
+    //console.log(myData)
+    myData.forEach(showPost)
+}
+
+function showPost(post){
+    console.log(post)
+    const template = document.querySelector(".postTemplate").content;
+    const postCopy = template.cloneNode(true);
+
+    const title = postCopy.querySelector(".project-title");
+    title.textContent = post.title.rendered;
+
+
+    const img = postCopy.querySelector("img.cover");
+    const imgPath = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    img.setAttribute("src", imgPath)
+    img.setAttribute("alt", "Poster for event" + post.title.rendered)
+
+    document.querySelector("#posts").appendChild(postCopy)
+}
