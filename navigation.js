9a65d35762bd8f3d56f5f02e7cd0a3ff392be271
Julia  Kulbaczewska <juliakulbaczewska@MacBook-Pro-Julia.local>
2019-12-13 16:22:58 +0100

    contact page done + responsive

--- navigation.js
+++ navigation.js
@@ -34,3 +34,45 @@ window.onscroll = function () {
     }
     prevScrollpos = currentScrollPos;
 }
+
+
+
+//Navigation for About page
+
+
+const navSlideContact = () => {
+    const burger = document.querySelector('.nav-burger');
+    const nav = document.querySelector('.nav-links-contact');
+    const navLinks = document.querySelectorAll('.nav-links-contact li');
+    //    Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        //     Burger Animation
+
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlideContact();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos || currentScrollPos < 100) {
+        document.getElementById("navbar-contact").style.top = "0";
+    } else {
+        document.getElementById("navbar-contact").style.top = "-7vh";
+    }
+    prevScrollpos = currentScrollPos;
+}
